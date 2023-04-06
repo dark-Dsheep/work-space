d16249b73c54f72a6cd73d76a5df7d733708a416
wawawa <1457480465@qq.com>
2023-04-06 19:04:13 +0800

    龟速幂

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 2;
+        int b = 5;
+        System.out.println(qpow(a, b));
+    }
+
+    static int qpow(int a, int b) {
+        int ans = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                ans = ans * a;
+            }
+            a *= a;
+            b >>= 1;
+        }
+        return ans;
     }
 }
