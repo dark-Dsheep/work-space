2abf7552f651c0ab2b3b7833df1edd0aa6a15474
wawawa <1457480465@qq.com>
2023-04-10 14:55:46 +0800

    2023/4/10更新Main.class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
         int a = 2;
         int b = 5;
-        int c = 10001;
         System.out.println(qpow(a, b));
+        System.out.println("ok");
     }
 
     static int qpow(int a, int b) {
@@ -18,4 +20,5 @@ public class Main {
         }
         return ans;
     }
+
 }
