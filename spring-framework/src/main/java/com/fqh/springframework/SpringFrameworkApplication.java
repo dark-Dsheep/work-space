434d829777d7e9f1d74a1f5165ecba2069f5595d
wawawa <1457480465@qq.com>
2023-04-24 16:44:04 +0800

    2023/4/24 重命名 SpringFrameworkApplication

--- spring-framework/src/main/java/com/fqh/springframework/SpringFrameworkApplication.java
+++ spring-framework/src/main/java/com/fqh/springframework/SpringFrameworkApplication.java
@@ -3,13 +3,12 @@ package com.fqh.springframework;
 import com.fqh.springframework.LifeCycle.BeanVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class MainApplication {
+public class SpringFrameworkApplication {
 
     public static void main(String[] args) {
-        var context = SpringApplication.run(MainApplication.class, args);
+        var context = SpringApplication.run(SpringFrameworkApplication.class, args);
         var bean = context.getBean(BeanVO.class);
 //        System.out.println(bean);
 
